bba3415378b0523eceea84ede21435d4d723fa18
ekater1na <ekaterina_l@tut.by>
2023-02-21 17:31:37 +0300

    feat: add container and basic styles

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,6 @@
+import style from "./Container.module.css";
+import classNames from "classnames";
+
+export const Container = ({ className }) => {
+  return <div className={classNames(style.container, className)}></div>;
+};
