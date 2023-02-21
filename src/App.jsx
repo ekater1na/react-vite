44f44475e77d949d0e8adbc955929b935a50cd77
ekater1na <ekaterina_l@tut.by>
2023-02-21 17:56:00 +0300

    feat: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Container } from "./components/Container/Container";
+import { Header } from "./components/Header/Header";
 
 export const App = () => {
   return (
     <>
-      <header>Header</header>
+      <header>
+        <Header />
+      </header>
       <main>
         <nav>
-          <Container className='navigation__container'/>
+          <Container className="navigation__container" />
         </nav>
         <section>
-        <Container className='navigation__container'/>
+          <Container className="navigation__container" />
         </section>
       </main>
       <footer></footer>
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,6 +1,8 @@
 import style from "./Container.module.css";
 import classNames from "classnames";
 
-export const Container = ({ className }) => {
-  return <div className={classNames(style.container, className)}></div>;
+export const Container = ({ children, className }) => {
+  return (
+    <div className={classNames(style.container, className)}>{children}</div>
+  );
 };
