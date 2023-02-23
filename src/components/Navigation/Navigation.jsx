caf8e8df27eb39034f37be94d6e1ff93acdcd895
ekater1na <ekaterina_l@tut.by>
2023-02-23 22:22:36 +0300

    feat: add api call for product

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,11 +2,20 @@ import classNames from "classnames";
 import style from "./Navigation.module.css";
 import { Container } from "../Container/Container";
 import { useDispatch, useSelector } from "react-redux";
-import { changeCategory } from "../../store/category/categorySlice";
+import {
+  categoryRequestAsync,
+  changeCategory,
+} from "../../store/category/categorySlice";
+import { useEffect } from "react";
+import { API_URI } from "../../const";
 
 export const Navigation = () => {
   const { category, activeCategory } = useSelector((state) => state.category);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(categoryRequestAsync("max"));
+  }, []);
 
   return (
     <nav className={style.navigation}>
@@ -19,9 +28,9 @@ export const Navigation = () => {
                   style.button,
                   activeCategory === i ? style.button_active : ""
                 )}
-                style={{ backgroundImage: `url(${item.image})` }}
+                style={{ backgroundImage: `url(${API_URI}/${item.image})` }}
                 onClick={() => {
-                  dispatch(changeCategory({indexCategory: i}));
+                  dispatch(changeCategory({ indexCategory: i }));
                 }}
               >
                 {item.rus}
